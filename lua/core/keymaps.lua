b94e58ad1c2d0cc4f6adb6896017ca1d1a56c4dc
elensiel <elensiel+github@proton.me>
2026-02-21 09:04:14 +0800

    migrated null-ls -> conform

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,5 +1,5 @@
 local map = vim.keymap.set
-local opts = { noremap = true, silent = true}
+local opts = { noremap = true, silent = true }
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
@@ -19,14 +19,10 @@ map("v", "<leader>q", ":q<CR>", opts)
 -- clipboard shit
 map("v", "<leader>y", '"+y', opts)
 map("n", "<leader>p", '"+p', opts)
+map("v", "<leader>p", '"+p', opts)
 
 -- fix whole file indent
 map("n", "<leader>=", 'ggVG=', opts)
 
--- found @ /plugins/ui.lua -> minimap
--- map('n', '<Leader>mc', MiniMap.close)
--- map('n', '<Leader>mf', MiniMap.toggle_focus)
--- map('n', '<Leader>mo', MiniMap.open)
--- map('n', '<Leader>mr', MiniMap.refresh)
--- map('n', '<Leader>ms', MiniMap.toggle_side)
--- map('n', '<Leader>mt', MiniMap.toggle)
+-- remove search highlight
+map("n", "<Esc>", "<cmd>nohlsearch<CR>")
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,14 +5,13 @@ return {
         dependencies = {
             "williamboman/mason.nvim",
             "williamboman/mason-lspconfig.nvim",
-            "jay-babu/mason-null-ls.nvim",              -- null-ls integration
-            "hrsh7th/nvim-cmp",                         -- completion engine
-            "hrsh7th/cmp-nvim-lsp",                     -- LSP source for nvim-cmp
-            "hrsh7th/cmp-buffer",                       -- buffer words source
-            "hrsh7th/cmp-path",                         -- filesystem paths source
-            "L3MON4D3/LuaSnip",                         -- snippet engine
-            "saadparwaiz1/cmp_luasnip",                 -- snippet completion source
-            "jose-elias-alvarez/null-ls.nvim",          -- linters and formatters
+            "hrsh7th/nvim-cmp",         -- completion engine
+            "hrsh7th/cmp-nvim-lsp",     -- LSP source for nvim-cmp
+            "hrsh7th/cmp-buffer",       -- buffer words source
+            "hrsh7th/cmp-path",         -- filesystem paths source
+            "L3MON4D3/LuaSnip",         -- snippet engine
+            "saadparwaiz1/cmp_luasnip", -- snippet completion source
+            "stevearc/conform.nvim"     -- formatter
         },
 
         build = ":MasonUpdate",
@@ -90,11 +89,12 @@ return {
                 require("mason-lspconfig").setup({
                     automatic_installation = true,
                     ensure_installed = {
-                        "lua_ls", -- lua
+                        "lua_ls",        -- lua
                         "powershell_es", -- ps
                         -- "clangd", -- cpp
-                        "jdtls", -- java
-                        "tsserver", -- typescript
+                        "cssls",
+                        "jdtls",  -- java
+                        "ts_ls",  -- typescript
                         "svelte", -- svelte, duh
                     },
 
@@ -102,26 +102,26 @@ return {
                     handlers = {
                         function(server_name)
                             lspconfig[server_name].setup {
-                                capabilities = capabilities
+                                capabilities = capabilities,
+                                on_attach = function(client)
+                                    client.server_capabilities.documentFormattingProvider = false
+                                end
                             }
                         end,
 
                         -- svelte
-                        ["svelte"] = function ()
+                        ["svelte"] = function()
                             lspconfig.svelte.setup({
                                 capabilities = capabilities,
-                                settings = {
-                                    svelte = {
-                                        plugin = {
-                                            compilerOptions = { dev = true }
-                                        }
-                                    }
-                                },
                                 on_attach = function(client)
+                                    client.server_capabilities.documentFormattingProvider = false
+
                                     vim.api.nvim_create_autocmd("BufWritePost", {
-                                        pattern = { "*.js", "*.ts"},
+                                        pattern = { "*.js", "*.ts" },
                                         callback = function()
-                                            client.notify("$/onDidChangeTsOrJsFile", { uri = vim.uri_from_bufnr(0)})
+                                            client.notify("$/onDidChangeTsOrJsFile", {
+                                                uri = vim.uri_from_bufnr(0)
+                                            })
                                         end,
                                     })
                                 end,
@@ -132,53 +132,27 @@ return {
             end
 
             local function setup_linter_formatter()
-                local null_ls = require("null-ls")
-                local mason_null_ls = require("mason-null-ls")
+                local conform = require("conform")
 
-                -- linter/formatter to install
-                local tools = {
-                    formatting = {
-                        "stylua", -- lua
-                        -- "clang-format", -- cpp
-                        "prettierd", -- prettier
+                conform.setup({
+                    formatters_by_ft = {
+                        -- lua = { "stylua" },
+                        javascript = { "prettierd", "prettier" },
+                        typescript = { "prettierd", "prettier" },
+                        javascriptreact = { "prettierd", "prettier" },
+                        typescriptreact = { "prettierd", "prettier" },
+                        svelte = { "prettierd", "prettier" },
+                        css = { "prettierd", "prettier" },
+                        html = { "prettierd", "prettier" },
+                        json = { "prettierd", "prettier" },
+                        markdown = { "prettierd", "prettier" },
                     },
-                    diagnostics = {
-                        "luacheck", -- lua
-                        -- "cpplint", -- cpp
-                    }
-                }
 
-                mason_null_ls.setup({
-                    automatic_installation = true,
-                    ensure_installed = vim.list_extend(tools.formatting, tools.diagnostics),
-                })
-
-                -- build sources
-                local sources = {}
-                for _, tool in ipairs(tools) do
-                    if null_ls.builtins.formatting[tool] then
-                        table.insert(sources, null_ls.builtins.formatting[tool])
-                    elseif null_ls.builtins.diagnostics[tool] then
-                        table.insert(sources, null_ls.builtins.diagnostics[tool])
-                    else
-                        vim.notify("[null-ls] Unknown tool: " .. tool, vim.log.levels.WARN)
-                    end
-                end
-
-                -- setup autoformat that supports it
-                null_ls.setup({
-                    sources = sources,
-                    on_attach = function(client, bufnr)
-                        if client.supports_method and client.supports_method("textDocument/formatting") then
-                            vim.api.nvim_clear_autocmds({ buffer = bufnr })
-                            vim.api.nvim_create_autocmd("BufWritePre", {
-                                buffer = bufnr,
-                                callback = function()
-                                    vim.lsp.buf.format({ bufnr = bufnr })
-                                end,
-                            })
-                        end
-                    end,
+                    format_on_save = {
+                        timeout_ms = 500,
+                        lsp_fallback = true,
+                    },
+                    -- cwd = "buffer",
                 })
             end
 
