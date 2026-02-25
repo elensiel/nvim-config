a7359c36566f9a79d0c6281d92362977ebfeb99b
elensiel <elensiel+github@proton.me>
2026-02-25 21:41:34 +0800

    telescope keymap [live_grep -> git_files] since live grep aint working as intended

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,10 +1,15 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	--tag = '0.1.8',
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		-- { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	},
 	keys = {
 		{ "<leader>ff", "<cmd>Telescope find_files<CR>" },
-		{ "<leader>fg", "<cmd>Telescope live_grep<CR>" },
+		{ "<leader>fg", "<cmd>Telescope git_files<CR>" },
+		-- { "<leader>fb", "<cmd>Telescope buffers<CR>" },
+		-- { "<leader>fg", "<cmd>Telescope live_grep<CR>" },
 	},
 	config = function()
 		require("telescope").setup({
