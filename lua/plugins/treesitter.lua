ea20dfff1832c527086124797fc3f9052ea914f1
elensiel <elensiel+github@proton.me>
2026-02-25 21:18:49 +0800

    tried to fix treesitter not sitting but failed in great sorrow.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,48 +1,40 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    lazy = false;
-    build = ":TSUpdate",
-    config = function()
-        require("nvim-treesitter.config").setup({
-            -- A list of parser names, or "all"
-            ensure_installed = {
-                "svelte", "javascript", "html", "css", "typescript",
-                "vimdoc", "lua", "bash", "java", "powershell", "c_sharp"
-                -- "cpp",
-            },
+	"nvim-treesitter/nvim-treesitter",
+	dependencies = {
+		"windwp/nvim-ts-autotag", --auto pair alt
+	},
+	lazy = false,
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.config").setup({
+			highlight = { enable = true },
+			indent = { enable = true },
 
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
+			sync_install = true,
+			auto_install = true,
+			ensure_installed = {
+				"svelte",
+				"javascript",
+				"html",
+				"css",
+				"typescript",
+				"vimdoc",
+				"lua",
+				"bash",
+				"powershell",
+				"java",
+				"c_sharp",
+			},
 
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
-            auto_install = true,
-
-            indent = {
-                enable = true
-            },
-
-            highlight = {
-                -- `false` will disable the whole extension
-                enable = true,
-
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
-                additional_vim_regex_highlighting = { "markdown" },
-            },
-        })
-
-        -- local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-        -- treesitter_parser_config.templ = {
-        -- install_info = {
-        -- url = "https://github.com/vrischmann/tree-sitter-templ.git",
-        -- files = {"src/parser.c", "src/scanner.c"},
-        -- branch = "master",
-        -- },
-        -- }
-        -- 
-        -- vim.treesitter.language.register("templ", "templ")
-    end
+			-- incremental_selection = {
+			-- 	enable = true,
+			-- 	keymaps = {
+			-- 		init_selection = "gnn", -- start selection
+			-- 		node_incremental = "grn", -- expand to next node
+			-- 		scope_incremental = "<leader>grc", -- expand to scope
+			-- 		node_decremental = "grm", -- shrink
+			-- 	},
+			-- },
+		})
+	end,
 }
