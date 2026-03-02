3eb6eb62e0ad9649cb05ca36ab280de4642ff8b0
elensiel <elensiel@proton.me>
2026-03-02 10:25:04 +0800

    added manual formatting call keybind

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -5,24 +5,20 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- back to netrw
-map("n", "<leader>e", ":Ex<CR>", opts)
-map("v", "<leader>e", ":Ex<CR>", opts)
+map({ "n", "v" }, "<leader>e", ":Ex<CR>", opts)
 
 -- write
-map("n", "<leader>w", ":w<CR>", opts)
-map("v", "<leader>w", ":w<CR>", opts)
+map({ "n", "v" }, "<leader>w", ":w<CR>", opts)
 
 -- quit. just quit.
-map("n", "<leader>q", ":q<CR>", opts)
-map("v", "<leader>q", ":q<CR>", opts)
+map({ "n", "v" }, "<leader>q", ":q<CR>", opts)
 
 -- clipboard shit
 map("v", "<leader>y", '"+y', opts)
-map("n", "<leader>p", '"+p', opts)
-map("v", "<leader>p", '"+p', opts)
+map({ "n", "v" }, "<leader>p", '"+p', opts)
 
 -- fix whole file indent
-map("n", "<leader>=", 'ggVG=', opts)
+-- map("n", "<leader>=", 'ggVG=', opts)
 
 -- remove search highlight
 map("n", "<Esc>", "<cmd>nohlsearch<CR>")
