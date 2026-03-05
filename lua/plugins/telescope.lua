30432faaaea2b2b211bd8c6af65d17d22362df8f
elensiel <elensiel@proton.me>
2026-03-05 13:56:34 +0800

    better telescope picker args

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,25 +1,54 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	--tag = '0.1.8',
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		-- { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-	},
-	keys = {
-		{ "<leader>ff", "<cmd>Telescope find_files<CR>" },
-		{ "<leader>fg", "<cmd>Telescope git_files<CR>" },
-		-- { "<leader>fb", "<cmd>Telescope buffers<CR>" },
-		-- { "<leader>fg", "<cmd>Telescope live_grep<CR>" },
-	},
-	config = function()
-		require("telescope").setup({
-			defaults = {
-				vimgrep_arguments = {
-					"rg",
-					"--hidden",
-					"!.git/*",
-				},
-			},
-		})
-	end,
+    'nvim-telescope/telescope.nvim',
+
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+
+        -- sorter (optional)
+        -- { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
+
+    keys = {
+        { "<leader>ff", "<cmd>Telescope find_files<CR>" },
+        { "<leader>fg", "<cmd>Telescope git_files<CR>" },
+        { "<leader>fb", "<cmd>Telescope live_grep<CR>" },
+    },
+
+    config = function()
+        require("telescope").setup({
+            defaults = {
+                mappings = {
+                    i = {
+                        -- clear prompt
+                        ['<C-u>'] = false,
+                    },
+                },
+
+                vimgrep_arguments = {
+                    "rg",
+                    "--color=never",
+                    "--no-heading",
+                    "--with-filename",
+                    "--line-number",
+                    "--column",
+                    "--smart-case",
+                    -- "--hidden",
+                    "--glob", "!.git/*",
+                },
+            },
+
+            pickers = {
+                find_files = {
+                    find_command = {
+                        "fd",
+                        "--type", "f",
+                        "--hidden",
+                        "--follow", -- follow symlinks (optional)
+                        "--strip-cwd-prefix",
+                        "--exclude", ".git",
+                    }
+                },
+            },
+        })
+    end,
 }
