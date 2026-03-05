3287a3364242333256ef3f4a60612002bdd7f5bd
elensiel <elensiel@proton.me>
2026-03-05 14:07:21 +0800

    merged 'treesitter.lua' and 'theme.lua' -> 'appearance.lua'

---
+++ lua/plugins/appearance.lua
@@ -0,0 +1,60 @@
+return {
+    -- colorscheme
+    {
+        "aktersnurra/no-clown-fiesta.nvim",
+        priority = 1000,
+        lazy = false,
+        config = function()
+            require("no-clown-fiesta").setup({
+                theme = "dim",
+
+                -- enabled for floating window transparency
+                -- ex. lazy, mason, telescope
+                transparent = true,
+                styles = {
+                    type = { bold = true },
+                    lsp = { underline = true },
+                    match_paren = { underline = true },
+                },
+            })
+
+            --activate
+            vim.cmd("colorscheme no-clown-fiesta")
+
+            -- set bg color to pure black
+            vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+        end,
+    },
+
+    -- syntax highlighting
+    {
+        "nvim-treesitter/nvim-treesitter",
+        dependencies = {
+            "windwp/nvim-ts-autotag", -- html auto tag
+        },
+        lazy = false,
+        build = ":TSUpdate",
+        config = function()
+            local language = {
+                "lua",
+                "powershell",
+                "svelte",
+                "html",
+                "css",
+                "typescript",
+            }
+
+            -- ensure installation
+            require("nvim-treesitter").install(language)
+
+            -- activate on what file
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = language,
+                callback = function()
+                    vim.treesitter.start()
+                end,
+            })
+        end,
+    },
+}
