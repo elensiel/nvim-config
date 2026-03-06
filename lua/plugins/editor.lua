07f72e73e50b9ed1c795ab3207887c283489c581
elensiel <elensiel@proton.me>
2026-03-06 16:41:12 +0800

    segregate completion plugins -> 'editor.lua' && created a 'languages' table on 'lsp.lua' for easier config of lsp and formatter for a specified language

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,26 +1,86 @@
 return {
-    -- set default settings to a more practical value
-    {
-        'tpope/vim-sensible',
-        lazy = false,
-    },
+	-- set default settings to a more practical value
+	{
+		"tpope/vim-sensible",
+		lazy = false,
+	},
 
-    -- surrounding pair shortcut
-    -- add, delete, change
-    {
-        'tpope/vim-surround',
-        event = "InsertEnter"
+	-- surrounding pair shortcut
+	-- add, delete, change
+	{
+		"tpope/vim-surround",
+		event = "InsertEnter",
 
-        -- actions
-        -- 'yss' -> add
-        -- 'cs' -> change
-        -- 'ds' -> delete
+		-- actions
+		-- 'yss' -> add
+		-- 'cs' -> change
+		-- 'ds' -> delete
 
-        -- USAGE
-        -- add on current line
-        -- 'yss<pair>'
+		-- USAGE
+		-- add on current line
+		-- 'yss<pair>'
 
-        -- add on buffer word
-        -- 'ysiw<pair>'
-    },
+		-- add on buffer word
+		-- 'ysiw<pair>'
+	},
+
+	-- completion
+	{
+		"hrsh7th/nvim-cmp", -- completion engine
+
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp", -- LSP source for nvim-cmp
+			"hrsh7th/cmp-buffer", -- buffer words source
+			"hrsh7th/cmp-path", -- filesystem paths source
+			"L3MON4D3/LuaSnip", -- snippet engine
+			"saadparwaiz1/cmp_luasnip", -- snippet completion source
+			"stevearc/conform.nvim", -- formatter
+		},
+
+		config = function()
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+
+				mapping = cmp.mapping.preset.insert({
+					["<C-j>"] = cmp.mapping.select_next_item(),
+					["<C-k>"] = cmp.mapping.select_prev_item(),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-y>"] = cmp.mapping.confirm({ select = true }),
+					["<C-e>"] = cmp.mapping.abort(),
+				}),
+
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+				}, {
+					{ name = "buffer" },
+					{ name = "path" },
+				}),
+			})
+
+			-- Command-line completion
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = "path" },
+					{ name = "cmdline" },
+				},
+			})
+
+			-- path completion
+			cmp.setup.cmdline("/", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = "buffer" },
+				},
+			})
+		end,
+	},
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,173 +1,119 @@
 return {
+	-- lsp + formatter
+	-- NOTE -> why formatter is here?
+	-- easy config for each language
+	-- via 'languages' table
 	{
-		"neovim/nvim-lspconfig",
+		"neovim/nvim-lspconfig", -- lsp config and launcher
 
 		dependencies = {
-			"williamboman/mason.nvim",
-			"williamboman/mason-lspconfig.nvim",
-			"hrsh7th/nvim-cmp", -- completion engine
-			"hrsh7th/cmp-nvim-lsp", -- LSP source for nvim-cmp
-			"hrsh7th/cmp-buffer", -- buffer words source
-			"hrsh7th/cmp-path", -- filesystem paths source
-			"L3MON4D3/LuaSnip", -- snippet engine
-			"saadparwaiz1/cmp_luasnip", -- snippet completion source
+			"williamboman/mason.nvim", -- lsp installer
+			"williamboman/mason-lspconfig.nvim", -- lsp and mason connector
 			"stevearc/conform.nvim", -- formatter
 		},
 
-		build = ":MasonUpdate",
-
 		config = function()
+			local languages = {
+				-- core
+				lua = { server = "lua_ls", formatter = { "prettierd", "prettier" } },
+				powershell = { server = "powershell_es", formatter = nil },
+				java = { server = "jdtls", formatter = { "prettierd", "prettier" } },
+
+				-- web dev
+				html = { server = "html", formatter = { "prettierd", "prettier" } },
+				css = { server = "cssls", formatter = { "prettierd", "prettier" } },
+				-- javascript = { server = "tsserver", formatter = { "prettierd", "prettier" } },
+				typescript = { server = "ts_ls", formatter = { "prettierd", "prettier" } },
+				svelte = { server = "svelte", formatter = { "prettierd", "prettier" } },
+
+				-- misc
+				json = { server = nil, formatter = { "prettierd", "prettier" } },
+				markdown = { server = "marksman", formatter = { "prettierd", "prettier" } },
+			}
+
+			vim.diagnostic.config({
+				virtual_text = true,
+				signs = true,
+				underline = true,
+				update_in_insert = false,
+				severity_sort = true,
+			})
+
+			require("mason").setup()
+
 			local lspconfig = vim.lsp.config
+			local capabilities = vim.tbl_deep_extend(
+				"force",
+				{},
+				vim.lsp.protocol.make_client_capabilities(),
+				require("cmp_nvim_lsp").default_capabilities()
+			)
 
-			local function setup_diagnostics()
-				vim.diagnostic.config({
-					virtual_text = true,
-					signs = true,
-					underline = true,
-					update_in_insert = false,
-					severity_sort = true,
-				})
+			local servers = {}
+			for _, lang in pairs(languages) do
+				if lang.server then
+					table.insert(servers, lang.server)
+				end
 			end
 
-			local function setup_completion()
-				local cmp = require("cmp")
-				local luasnip = require("luasnip")
+			require("mason-lspconfig").setup({
+				ensure_installed = servers,
 
-				cmp.setup({
-					snippet = {
-						expand = function(args)
-							luasnip.lsp_expand(args.body)
-						end,
-					},
-					mapping = cmp.mapping.preset.insert({
-						["<C-j>"] = cmp.mapping.select_next_item(),
-						["<C-k>"] = cmp.mapping.select_prev_item(),
-						["<C-Space>"] = cmp.mapping.complete(),
-						["<C-y>"] = cmp.mapping.confirm({ select = true }),
-						["<C-n>"] = cmp.mapping.abort(),
-					}),
-					sources = cmp.config.sources({
-						{ name = "nvim_lsp" },
-						{ name = "luasnip" },
-					}, {
-						{ name = "buffer" },
-						{ name = "path" },
-					}),
-				})
-
-				-- Command-line completion
-				cmp.setup.cmdline(":", {
-					mapping = cmp.mapping.preset.cmdline(),
-					sources = {
-						{ name = "path" },
-						{ name = "cmdline" },
-					},
-				})
-
-				cmp.setup.cmdline("/", {
-					mapping = cmp.mapping.preset.cmdline(),
-					sources = {
-						{ name = "buffer" },
-					},
-				})
-			end
-
-			local function setup_lsp()
-				require("mason").setup()
-
-				-- LSP capabilities
-				local cmp_lsp = require("cmp_nvim_lsp")
-				local capabilities = vim.tbl_deep_extend(
-					"force",
-					{},
-					vim.lsp.protocol.make_client_capabilities(),
-					cmp_lsp.default_capabilities()
-				)
-
-				-- install servers
-				require("mason-lspconfig").setup({
-					automatic_installation = true,
-					ensure_installed = {
-						"lua_ls", -- lua
-						"powershell_es", -- ps
-						-- "clangd", -- cpp
-						"cssls",
-						"jdtls", -- java
-						"ts_ls", -- typescript
-						"svelte", -- svelte, duh
-					},
-
-					-- setup installed servers
-					handlers = {
-						function(server_name)
-							lspconfig[server_name].setup({
-								capabilities = capabilities,
-								on_attach = function(client)
-									client.server_capabilities.documentFormattingProvider = false
-								end,
-							})
-						end,
-
-						-- svelte
-						["svelte"] = function()
-							lspconfig.svelte.setup({
-								capabilities = capabilities,
-								on_attach = function(client)
-									client.server_capabilities.documentFormattingProvider = false
-
-									vim.api.nvim_create_autocmd("BufWritePost", {
-										pattern = { "*.js", "*.ts" },
-										callback = function()
-											client.notify("$/onDidChangeTsOrJsFile", {
-												uri = vim.uri_from_bufnr(0),
-											})
-										end,
-									})
-								end,
-							})
-						end,
-					},
-				})
-			end
-
-			local function setup_linter_formatter()
-				local conform = require("conform")
-
-				conform.setup({
-					formatters_by_ft = {
-						lua = { "stylua" },
-						-- lua = { "prettierd", "prettier" },
-						javascript = { "prettierd", "prettier" },
-						typescript = { "prettierd", "prettier" },
-						javascriptreact = { "prettierd", "prettier" },
-						typescriptreact = { "prettierd", "prettier" },
-						svelte = { "prettierd", "prettier" },
-						css = { "prettierd", "prettier" },
-						html = { "prettierd", "prettier" },
-						json = { "prettierd", "prettier" },
-						markdown = { "prettierd", "prettier" },
-					},
-
-					format_on_save = {
-						timeout_ms = 1000,
-						lsp_fallback = true,
-					},
-
-					-- manual format keybind
-					vim.keymap.set({ "n", "v" }, "<leader>=", function()
-						conform.format({
-							async = true,
-							lsp_fallback = true,
+				-- setup installed servers
+				handlers = {
+					function(server_name)
+						lspconfig[server_name].setup({
+							capabilities = capabilities,
+							on_attach = function(client)
+								client.server_capabilities.documentFormattingProvider = false
+							end,
 						})
-					end),
-					-- cwd = "buffer",
-				})
+					end,
+
+					-- svelte override
+					["svelte"] = function()
+						lspconfig.svelte.setup({
+							capabilities = capabilities,
+							on_attach = function(client)
+								client.server_capabilities.documentFormattingProvider = false
+
+								vim.api.nvim_create_autocmd("BufWritePost", {
+									pattern = { "*.js", "*.ts" },
+									callback = function()
+										client.notify("$/onDidChangeTsOrJsFile", {
+											uri = vim.uri_from_bufnr(0),
+										})
+									end,
+								})
+							end,
+						})
+					end,
+				},
+			})
+
+			local conform = require("conform")
+			local formatters = {}
+			for ft, lang in pairs(languages) do
+				if lang.formatter then
+					formatters[ft] = lang.formatter
+				end
 			end
 
-			setup_diagnostics()
-			setup_completion()
-			setup_lsp()
-			setup_linter_formatter()
+			conform.setup({
+				formatters_by_ft = formatters,
+				format_on_save = {
+					timeout_ms = 1000,
+					lsp_fallback = true,
+				},
+
+				-- manual format keybind
+				vim.keymap.set({ "n", "v" }, "<leader>=", function()
+					conform.format({
+						async = true,
+						lsp_fallback = true,
+					})
+				end),
+			})
 		end,
 	},
 }
