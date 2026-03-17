d70dda237bb42b7ef84c8649afb68d710507d137
elensiel <elensiel@proton.me>
2026-03-17 11:38:49 +0800

    added decorated scrollbar

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,4 +1,19 @@
 return {
+	-- scrollbar (decorated)
+	{
+		-- much modern
+		"lewis6991/satellite.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = { "lewis6991/gitsigns.nvim" }, -- for git hunks
+
+		opts = {
+			handlers = {
+				marks = { enable = false },
+				quickfix = { enable = false },
+			},
+		},
+	},
+
 	-- status line
 	{
 		"itchyny/lightline.vim",
