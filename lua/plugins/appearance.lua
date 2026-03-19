7a990d40ea775f79c788e4d138c099458b6db84e
elensiel <elensiel@proton.me>
2026-03-19 11:41:14 +0800

    changed colorscheme from monochrome to tokynight-night

--- lua/plugins/appearance.lua
+++ lua/plugins/appearance.lua
@@ -1,13 +1,32 @@
 return {
 	-- colorscheme
 	{
-		repo = "fxn/vim-monochrome",
+		-- repo = "fxn/vim-monochrome",
+		-- config = function()
+		-- 	vim.cmd("colorscheme monochrome")
+		--
+		-- 	-- set bg color to pure black
+		-- 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		-- 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		-- end,
+
+		repo = "folke/tokyonight.nvim",
 		config = function()
-			vim.cmd("colorscheme monochrome")
+			vim.cmd("colorscheme tokyonight-night")
 
 			-- set bg color to pure black
 			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 		end,
+
+		-- repo = "navarasu/onedark.nvim",
+		-- opts = { ["tag"] = "v0.1.0" },
+		-- config = function()
+		-- 	vim.cmd("colorscheme onedark")
+		--
+		-- 	-- set bg color to pure black
+		-- 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		-- 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		-- end,
 	},
 }
